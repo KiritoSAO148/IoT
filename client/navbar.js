b29df78676ef245e4475f8d30ebcfba6d2bf0388
Buiqhuy258@ <wuochuy02@gmail.com>
2024-05-03 22:54:48 +0700

    fix navbar activation

--- client/navbar.js
+++ client/navbar.js
@@ -1,37 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const links = document.querySelectorAll("nav a");
+const navLinks = document.querySelectorAll(".nav-link");
 
-  links.forEach(function (link) {
-    link.addEventListener("click", function () {
-      links.forEach(function (otherLink) {
-        otherLink.classList.remove("active");
-      });
-
-      link.classList.add("active");
-
-      localStorage.setItem("selectedLink", link.getAttribute("href"));
-    });
-  });
-
-  highlightCurrentPage();
+navLinks.forEach((link) => {
+  console.log(link.href);
+  console.log(window.location.href);
+  if (link.href === window.location.href) {
+    link.classList.add("active");
+    link.setAttribute("aria-current", "page");
+  }
 });
-
-function highlightCurrentPage() {
-  let path = window.location.pathname;
-
-  path = path.replace(/^\/|\/$/g, "");
-
-  const links = document.querySelectorAll("nav a");
-
-  const selectedLink = localStorage.getItem("selectedLink");
-
-  links.forEach(function (link) {
-    let linkPath = link.getAttribute("href").replace(/^\/|\/$/g, "");
-    // console.log(linkPath);
-    // console.log(link);
-
-    if (linkPath === path || linkPath === selectedLink) {
-      link.classList.add("active");
-    }
-  });
-}
